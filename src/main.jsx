28909b3f9464889087efea00132f540307d115e3
tomdemilliano <tomdemilliano@gmail.com>
2026-02-05 13:05:38 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// Mock globals voor lokale ontwikkeling als ze niet bestaan
+if (typeof window.__firebase_config === 'undefined') {
+  window.__firebase_config = JSON.stringify({
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_ID",
+    appId: "YOUR_APP_ID"
+  });
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
