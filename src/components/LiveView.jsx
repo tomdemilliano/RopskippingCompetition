22650d48501c426128bdf546c5d73a116be86969
tomdemilliano <tomdemilliano@gmail.com>
2026-02-11 08:39:59 +0100

    toon enkel actieve wedstrijd, niet geselecteerde

--- src/components/LiveView.jsx
+++ src/components/LiveView.jsx
@@ -7,6 +7,7 @@ import { styles } from '../styles';
 
 const LiveView = ({ 
   selectedComp, // We gebruiken deze enkel als fallback of referentie
+  activeComp,
   activeEvent, 
   setActiveEvent, 
   activeReeks, 
